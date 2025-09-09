91b838473a7722468e69e704f472c58e22e1c975
las6nia <lalcalaroblero2@gmail.com>
2025-09-09 11:03:23 -0500

    Create class1.py

---
+++ ClassLectures/class1.py
@@ -0,0 +1,32 @@
+#this is my first coding project
+
+'''
+welcome to python class Fall
+2025
+
+print("I welcome you to my python code") #print is a method - it will user parenthesis()
+'''
+
+#response = input("write your name: ")
+
+#print('my','name','is ','Luis')
+'''
+name = input("what is your name: ")
+
+print("your name is",(name), end=" ")
+'''
+#values (everything is a string) - In exam '4'*'10'(get result), "4"+"7"(get result), '4'*7(get result)
+
+'''
+#value1 = int(input("enter your first value: "))
+#value2 = int(input("enter your second value: "))
+print(value1+value2)
+'''
+#problem a(b+c)
+
+value1= int(input("Enter first number: "))
+value2  = int(input("Enter second number: "))
+value3 = int(input("Enter a third number: "))
+print((value2+value3)*value1) #value2+value3 will add and will later multiply to value3 
+
+
