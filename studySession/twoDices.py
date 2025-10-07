bf6bb7b67e88bc4cab5a9d5547c4382e94c441a1
las6nia <lalcalaroblero2@gmail.com>
2025-10-07 09:37:39 -0500

    Create twoDices.py
    2 dice roll random generator

---
+++ studySession/twoDices.py
@@ -0,0 +1,18 @@
+import random
+import time
+
+diceOne= [1,2,3,4,5,6]
+diceTwo = [1,2,3,4,5,6]
+
+print("Welcome to the dice game!")
+time.sleep(1)
+roll = str(input("Enter 'r' to roll the dices"))
+
+#logic
+
+
+if roll == 'r':
+
+
+
+
