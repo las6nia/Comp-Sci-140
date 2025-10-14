75dcd255cfdca73625e95841ae4bb92e151fe568
las6nia <lalcalaroblero2@gmail.com>
2025-10-14 11:09:22 -0500

    Update class10Practice.py

--- ClassLectures/class10Practice.py
+++ ClassLectures/class10Practice.py
@@ -8,3 +8,24 @@ elif (num2 <= num1) and (num2 < num3):
     print(num2)
 else:
     print(num3)
+
+#class 11 - Loops
+
+#ZYBOOKS ASSIGNMENT
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+
+counter = 0
+
+if num1 % 2 > 0:
+    counter+=1
+if num2 %2 > 0:
+    counter+=1
+if num3 %2 > 0:
+    counter+=1
+if num4 %2 > 0:
+    counter+=1
+print(counter)
