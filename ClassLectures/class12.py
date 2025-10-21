798109e32b280f9580959174e5ac8e462fcfbed3
las6nia <lalcalaroblero2@gmail.com>
2025-10-21 09:53:01 -0500

    Implement average calculation for user inputs
    v1

---
+++ ClassLectures/class12.py
@@ -0,0 +1,10 @@
+nums = float(input("Enter num: "))
+totalSum =0 
+count = 0
+total = 0
+while num!=0:
+    count+=1
+    total+=num
+    num =float(input("enter num"))
+print(f"the average of the nums are {nums}")
+    
