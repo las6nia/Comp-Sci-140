03e42aed16ceb3be5810a718ae0387c8f422470d
Luis Alcala <lalcalaroblero2@gmail.com>
2025-12-09 10:17:24 -0600

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -35,7 +35,40 @@ class bankAccount:
     customerName = "" #inititalize as no name
     
     def __init__(self, name, bal): #this is how a constructor is built.... self means "look at the current object, in java is 'this'"
+       #instance attributes
         self.customerName = name #passing the customer name 
         self.balance = bal
+    def deposit(self, amt):
+        self.balance+=amt   
+                                                #Come back to the bottom code to add limits on free time!!!
+    def withdraw(self, amt):
+        if (self.balance > amt) and (self.balance-amt != 0): 
+            self.balance-=amt
+    def getDetails(self): #this is an instance of a method, we do not need to add parameter as it will gather all other details either way
+        print(f"Customer {self.customerName} has a balance of {self.balance}") 
+    #After every method - compile it so it works
+    
+acc1 = bankAccount("Luis", 500) #this takes a parameter, and it takes 3 values - this is how an object is created
+acc2 = bankAccount("Joe", 3000) #
+#How do I call instance method??
+print(acc1.getDetails())
+print(acc2.getDetails())
+
+#deposit
+acc1.deposit(100)
+print(acc1.getDetails())
+
+#Joe  - withdraw
+acc2.withdraw(2000)
+print(acc2.getDetails())
+#check this error, joe doesn't have more money
+acc2.withdraw(2001)
+print(acc2.getDetails())
+
+
+
+
+
+
 
 
