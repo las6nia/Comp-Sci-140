2e3417a7e9ffa269a6ce39a555a1db74b341a22c
Luis Alcala <lalcalaroblero2@gmail.com>
2025-12-09 09:49:22 -0600

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -1,1 +1,8 @@
                                 #Object Oriented Programming 
+#We use OOP to think of concept to write once and apply for all
+
+#Inventory class - It is not storing chocolate, different departments such as clothing and makeup - others, under inventory
+#University - all departments are an instance of the whole university, such as classrooms, 
+
+
+                                #An object belong to a class
