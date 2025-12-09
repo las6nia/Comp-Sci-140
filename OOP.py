0fb888751694c903ff8218bbec9a8cba26738ee9
Luis Alcala <lalcalaroblero2@gmail.com>
2025-12-09 09:57:28 -0600

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -6,3 +6,27 @@
 
 
                                 #An object belong to a class
+#class - family tree example
+#Object - create examples of the class
+#Attributes - properties of these object
+#Methods - things that are done belonging to that class
+'''
+class student: #add a colon, replaces curly braces that are done in java
+    #Attribute - first set of things
+    studentId, firstName, lastName #instance attributes
+    def enrollment():
+        pass #what does passs do?
+    def getHighSchool():
+        pass
+    def graduation():
+        pass
+
+class employee:
+    employeeId, employeeName, salary
+    def getEmployeeDetails():
+        pass
+    def
+
+#What is a constructor? a method that constructs a method for the class - in python = init
+'''
+
