b70be3193a677a6dab4174aac7d7423ba2e73e2b
Luis Alcala <lalcalaroblero2@gmail.com>
2025-12-17 22:49:46 -0600

    Create playing.py

---
+++ FreeTimePractice/playing.py
@@ -0,0 +1,62 @@
+def __init__(self, make, year, model, color):
+        self.make = make
+        self.year = year
+        self.model = model
+        self.color = color
+
+    def changeColors(self):
+     
+     return
+
+
+
+        
+
+
+class options():
+    if choice == 1:
+        newColor 
+        
+class menu():
+    print(f"{'Garage Manager':^30}")
+    print(f"{('='*15):>23}")
+    
+
+print(menu())
+
+
+class Car:
+    #Constructor
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model= model
+        self.year= year
+        self.color = color
+
+
+#Methods
+    def drive(self):
+        print("This car"+ self.model +" is driving")
+    
+    def stop(self):
+        print("This car "+ self.model +"is stopped")
+
+    #def _-init__ is the constructor
+car1 = Car("chevy", "Corvette", 2021, "red")
+
+print(car1.make)
+print(car1.model)
+print(car1.year)
+print(car1.color)
+
+car1.drive() #using methodws
+car1.stop() #using methodws
+
+car2 = Car("ford", "mustang", 2022, "red")
+print(car2.make)
+print(car2.model)
+print(car2.year)
+print(car2.color)
+
+car2.drive() #using methodws
+car2.stop() #using methodws
